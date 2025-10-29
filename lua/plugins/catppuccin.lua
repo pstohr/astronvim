1001acb8d3947fd283597964e45293032171ef71
pstohr <p.stohr@zanders.eu>
2025-10-29 19:03:28 +0100

    Add catppuccin configuration with more plugins

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,6 +2,103 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   opts = {
+    flavour = "mocha",
+    background = {
+      light = "latte",
+      dark = "mocha",
+    },
+    transparent_background = false,
+    show_end_of_buffer = false,
+    term_colors = true,
+    dim_inactive = {
+      enabled = false,
+      shade = "dark",
+      percentage = 0.15,
+    },
+    no_italic = false,
+    no_bold = false,
+    no_underline = false,
+    styles = {
+      comments = { "italic" },
+      conditionals = { "italic" },
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {},
+      operators = {},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    default_integrations = true,
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+      telescope = true,
+      lsp_trouble = true,
+      which_key = true,
+      indent_blankline = {
+        enabled = true,
+        scope_color = "",
+        colored_indent_levels = false,
+      },
+      mason = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+        },
+        inlay_hints = {
+          background = true,
+        },
+      },
+      navic = {
+        enabled = false,
+        custom_bg = "NONE",
+      },
+      dap = true,
+      dap_ui = true,
+      leap = true,
+      markdown = true,
+      neotest = true,
+      noice = true,
+      semantic_tokens = true,
+      treesitter_context = true,
+      ts_rainbow2 = true,
+      illuminate = true,
+      fidget = true,
+      flash = true,
+      hop = true,
+      dropbar = {
+        enabled = true,
+        color_mode = false,
+      },
+    },
+  },
+  config = function()
+    require("catppuccin").setup()
 
-  }
+    -- setup must be called before loading
+    vim.cmd.colorscheme "catppuccin"
+  end,
 }
