d255ea5bbcf0fb50d0481dce08c2ab4664bffa64
pstohr <p.stohr@zanders.eu>
2026-02-03 15:57:11 +0100

    Updated LSP configuration

---
+++ lua/plugins/mason-nvim-dap.lua
@@ -0,0 +1,27 @@
+return {
+  "jay-babu/mason-nvim-dap.nvim",
+  opts = {
+    handlers = {
+      python = function(source_name)
+        local dap = require("dap")
+        dap.adapters.python = {
+          type = "executable",
+          command = "/usr/bin/python3",
+          args = {
+            "-m",
+            "debugpy.adapter",
+          },
+        }
+
+        dap.configurations.python = {
+          {
+            type = "python",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}", -- This configuration will launch the current file if used.
+          },
+        }
+      end,
+    },
+  },
+}
