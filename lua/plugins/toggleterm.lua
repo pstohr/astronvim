bbad7cfd6c86a7c05a7d3ddfa18441ee6f5b87dc
pstohr <p.stohr@zanders.eu>
2026-05-27 09:16:59 +0200

    feat: Added vim commands to the toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,9 +3,57 @@ return {
     "akinsho/toggleterm.nvim",
     opts = {
       on_create = function(term)
-        term:send("%USERPROFILE%\\Apps\\CMDER\\vendor\\init.bat")
-        term:clear()
+        if vim.fn.has("win32") == 1 then
+          term:send("%USERPROFILE%\\Apps\\CMDER\\vendor\\init.bat")
+          term:clear()
+        end
       end,
+    },
+    keys = {
+       {
+        "jk",
+        [[<C-\><C-n>]],
+        mode = "t",
+        desc = "Exit terminal mode",
+      },
+      {
+        "<esc>",
+        [[<C-\><C-n>]],
+        mode = "t",
+        desc = "Exit terminal mode",
+      },
+      -- Window navigation mappings in terminal mode
+      {
+        "<C-h>",
+        [[<Cmd>wincmd h<CR>]],
+        mode = "t",
+        desc = "Go to left window",
+      },
+      {
+        "<C-j>",
+        [[<Cmd>wincmd j<CR>]],
+        mode = "t",
+        desc = "Go to lower window",
+      },
+      {
+        "<C-k>",
+        [[<Cmd>wincmd k<CR>]],
+        mode = "t",
+        desc = "Go to upper window",
+      },
+      {
+        "<C-l>",
+        [[<Cmd>wincmd l<CR>]],
+        mode = "t",
+        desc = "Go to right window",
+      },
+      -- Close window mapping in terminal mode
+      {
+        "<C-w>",
+        [[<C-d>]],
+        mode = "t",
+        desc = "Close window",
+      },
     }
   },
 }
